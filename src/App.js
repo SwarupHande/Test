11e9edb30f779584286f4fd412217a2343927194
Swarup Hande <71242592+RoyalKing6868@users.noreply.github.com>
2024-06-12 20:24:27 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 function App() {
+  const [steps, setSteps] = useState(1)
+  const [message,setMessage] = useState(messages[steps-1]);
+  const [isOpen,setIsOpen] = useState(true);
+  const next = () => {
+    if(steps<3){
+      setSteps(steps + 1);
+      setMessage(messages[steps+1])
+    }
+    else{
+      alert("Completed")
+    }
+    console.log(steps)
+  }
+  const prev = () => {
+    if(steps>1){
+      setSteps(steps - 1);
+    }
+    else{
+      alert("Click on Next button to Move Further");
+    }
+    console.log(steps)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button className="close" onClick={()=>setIsOpen(!isOpen)}>&times;</button>
+    {isOpen && (<div className="steps">
+      <div className="numbers">
+        <div className={steps>=1? "active" : ""}>1</div>
+        <div className={steps>=2? "active" : ""}>2</div>
+        <div className={steps>=3? "active" : ""}>3</div>
+      </div>
+
+      <p className="message">Step {steps}: {messages[steps-1]}</p>
+      <div className="buttons">
+        <button style={{background:'blue', color:'white' }} onClick={prev}>Prev</button>
+        <button style={{background:'blue', color:'white' }} onClick={next}>Next</button>
+      </div>
+    </div>)}
+    </>
   );
 }
 
